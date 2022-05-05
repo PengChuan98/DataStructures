8abefb592a396231eed2b4aa1a70e25e675f9340
chuan <1114003209@qq.com>
2022-05-05 23:42:21 +0800

    将常用变量抽出来

---
+++ src/structures/list/core.h
@@ -0,0 +1,24 @@
+/**
+ * @file core.h
+ * @author peng chuan (https://github.com/PengChuan98)
+ * @brief 一些核心的常用的数据结构
+ * @version 0.1
+ * @date 2022-05-05
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+typedef int ElementType;
+
+// set max size
+#define MAX_SIZE 100
+
+// set bool value
+typedef int bool;
+
+#define true 1
+#define false -1
+
+// set element type
+typedef int ElementType;
