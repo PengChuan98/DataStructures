137ee1488e948bf1ec73d1dbc4fe31e2bcd541a3
pengchuan <1114003209@qq.com>
2022-05-09 21:28:08 +0800

    修改后以适应linux下编译环境

--- src/c_toolkits/wintools.c
+++ src/c_toolkits/wintools.c
@@ -1,1 +1,23 @@
-#include "wintools.h"
+/**
+ * @file wintools.c
+ * @author peng chuan (https://github.com/PengChuan98)
+ * @brief some useful tools for windows
+ * @version 0.1
+ * @date 2022-05-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+// ! 判断是否是windows平台
+#if (WINVER >= 0x0600) 
+#ifndef _WINTOOLS_H_
+#define _WINTOOLS_H_
+
+
+#include <stdio.h>
+#include <Windows.h>
+
+
+#endif
+#endif
